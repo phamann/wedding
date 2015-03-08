ef727e49d8ce07e9349c2cb8f1749a2ffe38935c
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-03-08 09:59:00 +0000

    Change admin menu items

--- public/lib/modules/core/controllers/header.client.controller.js
+++ public/lib/modules/core/controllers/header.client.controller.js
@@ -6,25 +6,25 @@ import '../services/menus.client.service';
 export var header = angular.module('core.header.controller', ['core.menus.service']);
 
 header.controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	($scope, Authentication, Menus) => {
-		$scope.authentication = Authentication;
-		$scope.isCollapsed = false;
-		$scope.menu = Menus.getMenu('topbar');
+    ($scope, Authentication, Menus) => {
+        $scope.authentication = Authentication;
+        $scope.isCollapsed = false;
+        $scope.menu = Menus.getMenu('topbar');
 
-		//function(menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position)
+        //function(menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position)
 
-		Menus.addMenuItem('topbar', 'Location', 'location', 'item', 'location', true);
-		Menus.addMenuItem('topbar', 'Accomodation', 'accomodation', 'item', 'accomodation', true);
-		Menus.addMenuItem('topbar', 'Getting here', 'getting-here', 'item', 'gettinghere', true);
-		Menus.addMenuItem('topbar', 'Admin', 'admin', 'item', 'admin', false, ['admin']);
+        Menus.addMenuItem('topbar', 'Location', 'location', 'item', 'location', true);
+        Menus.addMenuItem('topbar', 'Accomodation', 'accomodation', 'item', 'accomodation', true);
+        Menus.addMenuItem('topbar', 'Getting here', 'getting-here', 'item', 'gettinghere', true);
+        Menus.addMenuItem('topbar', 'Users', 'users', 'item', 'users', false, ['admin']);
 
-		$scope.toggleCollapsibleMenu = () => {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};		
+        $scope.toggleCollapsibleMenu = () => {
+            $scope.isCollapsed = !$scope.isCollapsed;
+        };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', () => {
-			$scope.isCollapsed = false;
-		});
-	}
+        // Collapsing the menu after navigation
+        $scope.$on('$stateChangeSuccess', () => {
+            $scope.isCollapsed = false;
+        });
+    }
 ]);
