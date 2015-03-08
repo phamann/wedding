943562a989dac2a84165af87c184ba8e67688274
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-03-08 09:57:06 +0000

    Tidy up user settings templates

--- public/lib/modules/users/controllers/settings.client.controller.js
+++ public/lib/modules/users/controllers/settings.client.controller.js
@@ -3,40 +3,45 @@
 export var settings = angular.module('users.settings.controller', []);
 
 settings.controller('SettingsController', ['$scope', '$http', '$location', 'User', 'Authentication',
-	function($scope, $http, $location, User, Authentication) {
-		$scope.user = Authentication.user;
+    function($scope, $http, $location, User, Authentication) {
+        $scope.user = Authentication.user;
 
-		// If user is not signed in then redirect back home
-		if (!$scope.user) $location.path('/');
+        // If user is not signed in then redirect back home
+        if (!$scope.user) $location.path('/');
 
-		// Update a user profile
-		$scope.updateUserProfile = function(isValid) {
-			if (isValid) {
-				$scope.success = $scope.error = null;
-				var user = new User($scope.user);
+        $scope.transportOptions = [
+            {label: 'Bus from town', value: 'bus'},
+            {label: 'Not needed', value: 'none'}
+        ];
 
-				user.$update(function(response) {
-					$scope.success = true;
-					Authentication.user = response;
-				}, function(response) {
-					$scope.error = response.data.message;
-				});
-			} else {
-				$scope.submitted = true;
-			}
-		};
+        // Update a user profile
+        $scope.updateUserProfile = function(isValid) {
+            if (isValid) {
+                $scope.success = $scope.error = null;
+                var user = new User($scope.user);
 
-		// Change user password
-		$scope.changeUserPassword = function() {
-			$scope.success = $scope.error = null;
+                user.$update(function(response) {
+                    $scope.success = true;
+                    Authentication.user = response;
+                }, function(response) {
+                    $scope.error = response.data.message;
+                });
+            } else {
+                $scope.submitted = true;
+            }
+        };
 
-			$http.post('/users/password', $scope.passwordDetails).success(function(response) {
-				// If successful show success message and clear form
-				$scope.success = true;
-				$scope.passwordDetails = null;
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-	}
+        // Change user password
+        $scope.changeUserPassword = function() {
+            $scope.success = $scope.error = null;
+
+            $http.post('/users/password', $scope.passwordDetails).success(function(response) {
+                // If successful show success message and clear form
+                $scope.success = true;
+                $scope.passwordDetails = null;
+            }).error(function(response) {
+                $scope.error = response.message;
+            });
+        };
+    }
 ]);
